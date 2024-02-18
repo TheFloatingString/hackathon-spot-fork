4a0c96b6fb3e86efcd11c75fe14001b6ab59dd85
Laurence Liang <laurence.w.liang@gmail.com>
2024-02-18 00:31:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -29,21 +29,29 @@ def main():
     with SpotController(username=SPOT_USERNAME, password=SPOT_PASSWORD, robot_ip=ROBOT_IP) as spot:
 
         time.sleep(2)
-
-        # Move head to specified positions with intermediate time.sleep
-        spot.move_head_in_points(yaws=[0.2, 0],
-                                 pitches=[0.3, 0],
-                                 rolls=[0.4, 0],
-                                 sleep_after_point_reached=1)
-        time.sleep(3)
-
-        # Make Spot to move by goal_x meters forward and goal_y meters left
         spot.move_to_goal(goal_x=0.5, goal_y=0)
-        time.sleep(3)
+        time.sleep(1)
+        spot.move_to_goal(goal_x=-0.5,goal_y=0)
+        time.sleep(1)
+        spot.move_by_velocity_control(v_x=0,v_y=0,v_rot=0.3,cmd_duration=5)
+        time.sleep(1)
+        spot.move_by_velocity_control(v_x=0,v_y=0,v_rot=-0.3,cmd_duration=5)
+        time.sleep(1)
 
-        # Control Spot by velocity in m/s (or in rad/s for rotation)
-        spot.move_by_velocity_control(v_x=-0.3, v_y=0, v_rot=0, cmd_duration=2)
-        time.sleep(3)
+        # # Move head to specified positions with intermediate time.sleep
+        # spot.move_head_in_points(yaws=[0.2, 0],
+        #                          pitches=[0.3, 0],
+        #                          rolls=[0.4, 0],
+        #                          sleep_after_point_reached=1)
+        # time.sleep(3)
+
+        # # Make Spot to move by goal_x meters forward and goal_y meters left
+        # spot.move_to_goal(goal_x=0.5, goal_y=0)
+        # time.sleep(3)
+
+        # # Control Spot by velocity in m/s (or in rad/s for rotation)
+        # spot.move_by_velocity_control(v_x=-0.3, v_y=0, v_rot=0, cmd_duration=2)
+        # time.sleep(3)
 
 
 if __name__ == '__main__':
